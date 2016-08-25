3ac17f08e34707cd8960a8b41ce90136d70d9b58
sniggitysnax <dschnacky10@gmail.com>
2016-08-25 11:09:46 -0500

    Remove x-powered-by header

--- src/server/webapp.js
+++ src/server/webapp.js
@@ -10,6 +10,7 @@ let app = express();
 // Configure view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('x-powered-by', false);
 
 // Configure middleware
 app.use(bodyParser.urlencoded({ extended: false }));
