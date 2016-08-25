43fa6f476af0092bf6f7b7dd1731d5187047a2f4
Chetan Sachdev <cksachdev@gmail.com>
2016-08-25 21:31:20 +0530

    fix for Issue #7

--- src/server/controllers/routes.js
+++ src/server/controllers/routes.js
@@ -1,9 +1,10 @@
 const todos = require('./todos');
+const changeheaders = require('./changeheaders');
 
 // Define handlers for application routes
 module.exports = (app) => {
   app.get('/', todos.index);
-  app.get('/todos', todos.all);
+  app.get('/todos', [changeheaders(), todos.all] );
   app.post('/todos', todos.create);
   app.put('/todos/:id', todos.update);
   app.delete('/todos/:id', todos.remove);
