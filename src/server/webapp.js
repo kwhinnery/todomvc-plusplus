b7e5d58968796ef5389fb569ce0ddf1cf2f7e592
Chetan Sachdev <cksachdev@gmail.com>
2016-08-25 21:46:48 +0530

    Fixes for exercise 1

--- src/server/webapp.js
+++ src/server/webapp.js
@@ -4,14 +4,17 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./controllers/routes');
+const morgan = require('morgan');
 
 let app = express();
 
 // Configure view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+app.set('x-powered-by', false);
 // Configure middleware
+
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Static file serving happens everywhere but in production
